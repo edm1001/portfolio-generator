092b838b0d5a856273456228a494879221f74cc2
edm1001 <edmer_franciz@yahoo.com>
2023-01-21 15:26:08 -0600

    create utils dir

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-import fs from 'fs';
-import generatePage from './src/page-template.js';
-import inquirer from 'inquirer';
+const fs = require('fs');
+// import generatePage from './src/page-template.js';
+const generatePage = require('./src/page-template.js')
+// import generatePage from './src/page-template.js';
+// import inquirer from 'inquirer';
+const inquirer = require('inquirer')
 const promptUser = () => {
   return inquirer.prompt([
     {
@@ -124,18 +127,40 @@ Add a New Project
       }
     });
 };
-
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
- const pageHTML = generatePage(portfolioData);
-
-   
-    fs.writeFile('./index.html', pageHTML, err => {
-      if (err) throw new Error(err);
-      console.log('Page created! Check out index.html in this directory to see it!');
-    });
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
   });
+
+fs.writeFile('./dist/index.html', pageHTML, err => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log('Page created! Check out index.html in this directory to see it!');
+
+  fs.copyFile('./src/style.css', './dist/style.css', err => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log('Style sheet copied successfully!');
+  });
+});
 
     // const fs = require('fs');
 // const pageHTML = generatePage(name, github);
--- src/page-template.js
+++ src/page-template.js
@@ -55,9 +55,8 @@ const generateProjects = projectsArr => {
 };
 
 
-module.exports = (name, github) => {
-  // destructure data by sections
-  const {projects, about, ...header } = templateData;
+module.exports = templateData => {
+ const {projects, about, ...header} = templateData
     return `
     <!DOCTYPE html>
     <html lang="en">
@@ -77,14 +76,13 @@ module.exports = (name, github) => {
         <div class="container flex-row justify-space-between align-center py-3">
           <h1 class="page-title text-secondary bg-dark py-2 px-3">${header.name}</h1>
           <nav class="flex-row">
-            <a class="ml-2 my-1 px-2 py-1 bg-secondary text-dark" href="https://github.com/${
-              header.github
-            }">GitHub</a>
+            <a class="ml-2 my-1 px-2 py-1 bg-secondary text-dark" href="https://github.com/${header.github}">GitHub</a>
           </nav>
         </div>
       </header>
       <main class="container my-5">
             ${generateAbout(about)}
+            ${generateProjects(projects)}
       </main>
       <footer class="container text-center py-3">
         <h3 class="text-dark">&copy; ${new Date().getFullYear()} by ${header.name}</h3>
